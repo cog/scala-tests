a686a114b56a3f3c1d93462cabf7cc575bd60b0a
José Castro <cog@cpan.org>
2015-10-06 13:24:06 +0100

    lexical closures

---
+++ lexical closure.scala
@@ -0,0 +1,17 @@
+def fun1():Int => Int = {
+	val y = 1
+	def add(x: Int) = x + y
+	
+	add
+}
+
+def fun2() = {
+	val y = 2
+	
+	// this function is still going to use the y value defined within fun1()
+	val f = fun1()
+	
+	println(f(10))
+}
+
+fun2()
