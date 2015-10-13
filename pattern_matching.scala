99bb19aa0d384f768ed88926a7b755fe60327b2b
José Castro <cog@cpan.org>
2015-10-13 16:54:25 +0100

    length of lists

--- pattern_matching.scala
+++ pattern_matching.scala
@@ -11,3 +11,11 @@ def fun2(a: List[Int]) = a match {
 	case List(0, p, q) => p + q
 	case List() => -1
 }
+
+def length(a: List[Int]):Int = a match {
+	case Nil => 0
+	case h::t => 1 + length(t)
+}
+
+println(length(List()))
+println(length(List(10,20,30,40)))
