63be1b98bec30a427336b2baeee74244765a809b
José Castro <cog@cpan.org>
2015-10-13 16:56:57 +0100

    sumTree

--- binary_tree.scala
+++ binary_tree.scala
@@ -8,3 +8,11 @@ val a = Node(20, Leaf(10), Leaf(30))
 val b = Node(40, Leaf(37), Leaf(50))
 val c = Node(35, a, b)
 println(c)
+
+def sumTree(t: Tree): Int = t match {
+	case Leaf(value) => value
+	case Node(value, left, right) => value + sumTree(left) + sumTree(right)
+}
+
+println(sumTree(c))
+println(sumTree(Node(10, Leaf(5), Leaf(20))))
