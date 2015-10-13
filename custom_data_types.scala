e8e7b8e849a021be4e014732de26f17a8ae29b1f
José Castro <cog@cpan.org>
2015-10-13 16:54:37 +0100

    custom data types

--- custom_data_types.scala
+++ custom_data_types.scala
@@ -1,16 +1,37 @@
-case class Circle(r: Int)
+sealed abstract class Shape
 
-case class Rectangle(h: Int, w: Int)
+case class Circle(r: Int) extends Shape
+case class Rectangle(h: Int, w: Int) extends Shape
 
-val a = Circle(10)
-println(a.r) // radius of the circle is 10
+val a:Shape = Circle(10)
 
-val b = Rectangle(5, 7)
-println(b.h) // height of the rectangle is 5
+val b:Shape = Rectangle(10, 20)
 
 
-def area(c: Circle) = 3.14 * c.r * c.r
-def area(r: Rectangle) = r.h * r.w
+// def area(c: Circle) = 3.14 * c.r * c.r
+// def area(r: Rectangle) = r.h * r.w
 
+
+def area(a: Shape) = a match {
+	case Circle(r) => 3.14 * r * r
+	case Rectangle(h, w) => h * w
+}
+
+///////////////////////////////////////
+
+def area2(a: Circle) =
+	3.14 * a.r * a.r
+
+def area2(b: Rectangle) = 
+	b.h * b.w
+
+
+val a2 = Circle(10)
+
+val b2 = Rectangle(10, 20)
+
+println(area(a))
 println(area(b))
 
+println(area2(a2))
+println(area2(b2))
