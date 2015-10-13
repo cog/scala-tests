588b1267808b3686fb3381e6a0b4c33997b01da9
José Castro <cog@cpan.org>
2015-10-13 15:25:37 +0100

    pattern matching examples

---
+++ pattern_matching.scala
@@ -0,0 +1,13 @@
+def fun(a: List[Int]) = a match {
+	case List(0, p, q) => p + q
+	case _ => -1
+}
+
+println(fun(List(0, 10, 20)))
+println(fun(List(0, 1, 2, 3)))
+println(fun(List(1, 10, 20)))
+
+def fun2(a: List[Int]) = a match {
+	case List(0, p, q) => p + q
+	case List() => -1
+}
